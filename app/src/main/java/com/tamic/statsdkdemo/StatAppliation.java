d899f9f9030154bc31110be123fc48ef0860a64b
liuyongkui <liuyongkui726@pingan.com.cn>
2016-09-26 17:44:06 +0800

    某些优化

--- app/src/main/java/com/tamic/statsdkdemo/StatAppliation.java
+++ app/src/main/java/com/tamic/statsdkdemo/StatAppliation.java
@@ -15,10 +15,13 @@ public class StatAppliation extends Application {
         int appId = 21212;
         // assets
         String fileName = "stat_id.json";
+
         String url = "http://www.baidu.com";
         // init statSdk
         TcStatInterface.initialize(this, appId, "you app chanel", fileName);
+        // set upload url
         TcStatInterface.setUrl(url);
+        // Set loadPolicy
         TcStatInterface.setUploadPolicy(TcStatInterface.UploadPolicy.UPLOAD_POLICY_DEVELOPMENT, TcStatInterface.UPLOAD_TIME_ONE);
     }
 
